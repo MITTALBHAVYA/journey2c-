141d825b21003d307817427bb0c2f74cade7cb4c
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2023-09-17 13:37:47 +0530

    message not found

---
+++ ptanhi.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+void solve(){
+  
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+   cin.tie(0);
+  int test;
+  cin>>test;
+  while(test--)
+  {
+    solve();
+  }
+ return 0;
+}
