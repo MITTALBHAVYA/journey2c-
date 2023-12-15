779905e1acc523da9a4a0b0f448821e994c15b46
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2023-12-15 19:49:56 +0530

    update

--- code1.cpp
+++ code1.cpp
@@ -2,102 +2,46 @@
 #define ll long long
 #define mod 1000000007
 using namespace std;
-
-// Structure to store information about suffixes
-struct Suffix {
-    ll index;
-    std::string suffix;
-};
-
-// Function to compare two suffixes
-bool compareSuffixes(const Suffix &a, const Suffix &b) {
-    return a.suffix < b.suffix;
-}
-
-// Function to construct suffix array
-std::vector<ll> buildSuffixArray(const std::string &str) {
-    ll n = str.length();
-    std::vector<Suffix> suffixes(n);
-
-    // Generate suffixes of the string
-    for (ll i = 0; i < n; ++i) {
-        suffixes[i].index = i;
-        suffixes[i].suffix = str.substr(i);
-    }
-
-    // Sort the suffixes
-    std::sort(suffixes.begin(), suffixes.end(), compareSuffixes);
-
-    // Store the indexes of sorted suffixes in the suffix array
-    std::vector<ll> suffixArray(n);
-    for (ll i = 0; i < n; ++i) {
-        suffixArray[i] = suffixes[i].index;
-    }
-
-    return suffixArray;
-}
-
-// Function to construct LCP array
-std::vector<ll> buildLCPArray(const std::string &str, const std::vector<ll> &suffixArray) {
-    ll n = str.length();
-    std::vector<ll> lcp(n, 0);
-    std::vector<ll> invSuffixArray(n);
-
-    // Populate inverse suffix array
-    for (ll i = 0; i < n; ++i) {
-        invSuffixArray[suffixArray[i]] = i;
-    }
-
-    ll k = 0;
-    for (ll i = 0; i < n; ++i) {
-        if (invSuffixArray[i] == n - 1) {
-            k = 0;
-            continue;
-        }
-
-        ll j = suffixArray[invSuffixArray[i] + 1];
-        while (i + k < n && j + k < n && str[i + k] == str[j + k]) {
-            k++;
-        }
-
-        lcp[invSuffixArray[i]] = k;
-
-        if (k > 0) {
-            k--;
-        }
-    }
-
-    return lcp;
-}
-
-// Function to count distinct substrings
-ll countDistinctSubstrings(const std::string &str) {
-    ll n = str.length();
-    std::vector<ll> suffixArray = buildSuffixArray(str);
-    std::vector<ll> lcp = buildLCPArray(str, suffixArray);
-
-    ll distinctSubstrings = 0;
-    for (ll i = 0; i < n; ++i) {
-        distinctSubstrings += (n - suffixArray[i]);
-        if (i > 0) {
-            distinctSubstrings -= lcp[i - 1];
-        }
-        distinctSubstrings %= mod; // Apply modulo operation
-        if (distinctSubstrings < 0) {
-            distinctSubstrings += mod;
-        }
-    }
-
-    return distinctSubstrings;
-}
 void solve(){
-  ll n;
-  cin>>n;
-  string s;
-  cin>>s;
-  sort(s.begin(),s.end());
-  ll ans=countDistinctSubstrings(s);
-  cout<<ans<<endl;
+  ll n,k;
+  cin>>n>>k;
+  ll count_odd=0;
+  ll count_even=0;
+  ll sum=0;
+  vector<ll>arr(n);
+  ll mini=INT_MAX;
+  for(ll i=0;i<n;i++){
+    cin>>arr[i];
+    if(arr[i]%2==1){
+        count_odd++;
+    }
+    mini=min(mini,arr[i]);
+    sum+=arr[i];
+  }
+  count_even=n-count_odd;
+  if(count_odd>=k){
+    sum+=k;
+    cout<<sum<<endl;
+  }
+  else{
+    k=k-count_odd;
+    sum+=count_odd;
+    if(mini%2==1)
+    {
+        mini=mini+1;
+    }
+    sum-=mini;
+    while(k--){
+        if(mini%2==0){
+            mini/=2;
+        }
+        else{
+            mini++;
+        }
+    }
+    sum+=mini;
+    cout<<sum<<endl;
+  }
 }
 int main()
 {
