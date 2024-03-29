4f892ce379f9f3a5891ea377e441d42c4a62579d
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2024-03-29 11:41:57 +0530

    nimgame

---
+++ CSESfiles/nimgame1.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+void solve(){
+  ll n;
+  cin>>n;
+  vector<ll>arr(n);
+  for(ll i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  ll  ans=arr[0];
+  for(ll i=1;i<n;i++){
+    ans=ans^arr[i];
+  }
+  if(ans==0){
+    cout<<"second"<<endl;
+  }
+  else{
+    cout<<"first"<<endl;
+  }
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+   cin.tie(0);
+  int test;
+  cin>>test;
+  while(test--)
+  {
+    solve();
+  }
+ return 0;
+}
